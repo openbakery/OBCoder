70fca3b6a5deaee381316366705625a97c1a3a66
René Pirringer <rene@pirringer.at>
2025-05-09 11:22:18 +0200

    updated Package.swift so that "swift build" and "swift test" are working

--- Package.swift
+++ Package.swift
@@ -2,35 +2,35 @@
 import PackageDescription
 
 let package = Package(
-	name: "OBCoder",
-	platforms: [
-		.iOS(.v14)
-	],
-	products: [
-		.library(name: "OBCoder", targets: ["OBCoder"])
-	],
-	dependencies: [
-		.package(
-			url: "https://github.com/nschum/SwiftHamcrest/",
-			.upToNextMajor(from: "2.3.0")
-		)
-	],
-	targets: [
-		.target(
-			name: "OBCoder",
-			dependencies: [],
-			path: "Main",
-			sources: [
-				"Source"
-			]
-		),
-		.testTarget(
-			name: "OBCoderTests",
-			dependencies: ["OBCoder", "SwiftHamcrest"],
-			path: "Test",
-			sources: [
-				"Source"
-			]
-		)
-	]
+    name: "OBCoder",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "OBCoder", targets: ["OBCoder"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nschum/SwiftHamcrest/", .upToNextMajor(from: "2.3.0"))
+    ],
+    targets: [
+        .target(
+            name: "OBCoder",
+            dependencies: [],
+            path: "Main",
+            sources: [
+                "Source"
+            ]
+        ),
+        .testTarget(
+            name: "OBCoderTests",
+            dependencies: [
+                "OBCoder",
+                .product(name: "Hamcrest", package: "SwiftHamcrest"),
+            ],
+            path: "Test",
+            sources: [
+                "Source"
+            ]
+        ),
+    ]
 )
